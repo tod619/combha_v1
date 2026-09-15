7289523d52986c3f97a1da34053b4b124442cf31
tod619 <trevorodwyer619@hotmail.com>
2026-09-15 14:17:04 +0100

    Add functionality to navbar and fix bugs in script.js and style.css files

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,9 @@
+document.addEventListener('DOMContentLoaded', function () {
+  // Navbar mobile
+  const toggleButton = document.querySelector('.navbar__mobile-menu-toggle');
+  const mobileMenu = document.querySelector('.navbar__mobile-menu-items');
 
+  toggleButton.addEventListener('click', () => {
+    mobileMenu.classList.toggle('active');
+  });
+});
