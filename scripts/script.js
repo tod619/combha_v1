bebdfd80be47ee4de81e2718451f6ea5f29bf9bb
tod619 <trevorodwyer619@hotmail.com>
2026-09-15 14:44:29 +0100

    Add scrollfunctionality for navbar in script.js file

--- scripts/script.js
+++ scripts/script.js
@@ -7,3 +7,13 @@ document.addEventListener('DOMContentLoaded', function () {
     mobileMenu.classList.toggle('active');
   });
 });
+
+window.addEventListener('scroll', () => {
+  const navbar = document.querySelector('.navbar');
+
+  if (window.scrollY > 0) {
+    navbar.classList.add('navbar--scroll');
+  } else {
+    navbar.classList.remove('navbar--scroll');
+  }
+});
